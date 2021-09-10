a73cde2c236e7355f99f866b2e80980d049aeb3d
KulikSS0395 <ds_347@mail.ru>
2021-09-10 15:26:10 +0300

    1.1.3

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -2,84 +2,182 @@ package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
+
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
 
-    private static final String INSERT_NEW = "INSERT INTO users VALUES(default,?,?,?)";
-    private static final String CREAT_NEW_TABLE = "CREATE TABLE IF NOT EXISTS users (\n" +
-                                                    "id int PRIMARY KEY NOT NULL AUTO_INCREMENT,\n" +
-                                                    "name VARCHAR(45) NOT NULL,\n" +
-                                                    "lastNAME VARCHAR(45) NOT NULL,\n" +
-                                                    "age int NOT NULL\n" + ");";
 
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
-        try (Connection connection = Util.getConnection()) {
-            connection.createStatement().execute(CREAT_NEW_TABLE);
+        Connection connection = null;
+
+        try {
+            connection = Util.getConnection();
+            Statement statement = connection.createStatement();
+            statement.execute("CREATE TABLE IF NOT EXISTS users (\n" +
+                                    "id BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,\n" +
+                                    "name VARCHAR(45) NOT NULL,\n" +
+                                    "lastNAME VARCHAR(45) NOT NULL,\n" +
+                                    "age TINYINT NOT NULL\n" + ");");
+
+            try {
+                statement.close();
+            } catch (SQLException e) {
+                connection.rollback();
+                System.err.println("Failed close statement when create table");
+            }
+
+            connection.commit();
+
         } catch (SQLException e) {
+            System.err.println("Create table failed");
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (Exception e) {
+                System.err.println("Failed close connection");
+            }
         }
 
     }
 
     public void dropUsersTable() {
-        try (Connection connection = Util.getConnection()) {
-            connection.createStatement().execute("DROP TABLE users");
+        Connection connection = null;
+
+        try {
+            connection = Util.getConnection();
+            Statement statement = connection.createStatement();
+            statement.execute("DROP TABLE IF EXISTS users");
+
+            try {
+                statement.close();
+            } catch (SQLException e) {
+                connection.rollback();
+                System.err.println("Failed close statement when drop table");
+            }
+            connection.commit();
+
         } catch (SQLException e) {
+            System.err.println("Drop table failed");
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (Exception e) {
+                System.err.println("Failed close connection");
+            }
         }
     }
 
     public void saveUser(String name, String lastName, byte age) {
+        Connection connection = null;
 
-        try (Connection connection = Util.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(INSERT_NEW)) {
-
+        try {
+            connection = Util.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO users (name, lastNAME, age) VALUES(?,?,?)");
             preparedStatement.setString(1, name);
             preparedStatement.setString(2, lastName);
             preparedStatement.setInt(3, age);
-            preparedStatement.execute();
+            preparedStatement.executeUpdate();
+
+            try {
+                preparedStatement.close();
+            } catch (SQLException e) {
+                connection.rollback();
+                System.err.println("Failed close preparedStatement when save user");
+            }
+            connection.commit();
+
             System.out.println("User с именем - " + name + " добавлен в базу данных");
 
         } catch (SQLException e) {
-
+            System.err.println("Save user failed");
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (Exception e) {
+                System.err.println("Failed close connection");
+            }
         }
-
     }
 
     public void removeUserById(long id) {
-        try (Connection connection = Util.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM users where id = ?")) {
+        Connection connection = null;
 
+        try {
+            connection = Util.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM users where id = ?");
             preparedStatement.setInt(1, (int) id);
             preparedStatement.executeUpdate();
 
-        } catch (SQLException e) {
-        }
+            try {
+                preparedStatement.close();
+            } catch (SQLException e) {
+                connection.rollback();
+                System.err.println("Failed close preparedStatement when remove user");
+            }
+            connection.commit();
 
+        } catch (SQLException e) {
+            System.err.println("Remove user failed");
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (Exception e) {
+                System.err.println("Failed close connection");
+            }
+        }
     }
 
     public List<User> getAllUsers() {
+        Connection connection = null;
         List<User> users = new ArrayList<>();
 
-        try (Connection connection = Util.getConnection();
-             Statement statement = connection.createStatement();
-             ResultSet resultSet = statement.executeQuery("SELECT  * FROM users")) {
-
+        try {
+            connection = Util.getConnection();
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT  * FROM users");
             while (resultSet.next()) {
                 User user = new User(resultSet.getString("name"),
-                                        resultSet.getString("lastName"),
-                                        resultSet.getByte("age"));
+                        resultSet.getString("lastName"),
+                        resultSet.getByte("age"));
 
                 user.setId(resultSet.getLong("id"));
                 users.add(user);
             }
 
+            try {
+                statement.close();
+                resultSet.close();
+            } catch (SQLException e) {
+                connection.rollback();
+                System.err.println("Failed close statement when get user");
+            }
+            connection.commit();
+
         } catch (SQLException e) {
+            System.err.println("Get allUsers failed");
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (Exception e) {
+                System.err.println("Failed close connection");
+            }
         }
 
         return users;
@@ -87,10 +185,31 @@ public class UserDaoJDBCImpl implements UserDao {
 
 
     public void cleanUsersTable() {
-        try (Connection connection = Util.getConnection()) {
-            connection.createStatement().execute("DROP table users");
-            connection.createStatement().execute(CREAT_NEW_TABLE);
+        Connection connection = null;
+
+        try {
+            connection = Util.getConnection();
+            Statement statement = connection.createStatement();
+            statement.execute("TRUNCATE TABLE users");
+
+            try {
+                statement.close();
+            } catch (SQLException e) {
+                connection.rollback();
+                System.err.println("Failed close statement when create table");
+            }
+            connection.commit();
+
         } catch (SQLException e) {
+            System.err.println("Clean table failed");
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (Exception e) {
+                System.err.println("Failed close connection");
+            }
         }
 
     }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -9,14 +9,17 @@ public class Util {
     private static final String URL = "jdbc:mysql://localhost:3306/mydbtest";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "root";
-
     private static Connection connection;
 
     public static Connection getConnection() {
         try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
             connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            connection.setAutoCommit(false);
+        } catch (ClassNotFoundException e) {
+            System.err.println("Not download Driver mySQL");
         } catch (SQLException e) {
-            e.printStackTrace();
+            System.err.println("Failed connection");
         }
         return connection;
     }
